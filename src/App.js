8e7dd92bc6387b8f96c8447d13caa6a2af9354a1
Morgan <morgan@morganevans.dev>
2022-12-14 12:21:54 +0000

    add profile page and redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./logo.svg";
+import ProfilePage from "./components/ProfilePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/in/:user_id" element={<ProfilePage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
