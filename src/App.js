1aaf15c2d72a0e920c36eec73d4883bb262be3db
Morgan <morgan@morganevans.dev>
2022-12-15 07:49:23 +0000

    update navbar & added active user redux

--- src/App.js
+++ src/App.js
@@ -7,38 +7,55 @@ import "./navbar.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import SearchPage from "./components/SearchPage";
+import FeedPage from "./components/FeedPage";
 
 function App() {
-  const users = useSelector((state) => state.users);
-  const dispatch = useDispatch();
-  useEffect(async () => {
-    const response = await fetch(
-      "https://striveschool-api.herokuapp.com/api/profile/",
-      {
-        headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzk5YmUwZDU0ZjRhYTAwMTUxOTMwMzkiLCJpYXQiOjE2NzEwMjAwNDUsImV4cCI6MTY3MjIyOTY0NX0.3XuEFOEGaNXNTq32t5nPT9qEUWuQJirhYW-2WA-luyQ`,
-        },
-      }
+    const users = useSelector((state) => state.users);
+    const dispatch = useDispatch();
+    useEffect(async () => {
+        const response = await fetch(
+            "https://striveschool-api.herokuapp.com/api/profile/",
+            {
+                headers: {
+                    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzk5YmUwZDU0ZjRhYTAwMTUxOTMwMzkiLCJpYXQiOjE2NzEwMjAwNDUsImV4cCI6MTY3MjIyOTY0NX0.3XuEFOEGaNXNTq32t5nPT9qEUWuQJirhYW-2WA-luyQ`,
+                },
+            }
+        );
+        if (response.ok) {
+            const data = await response.json();
+            dispatch({
+                type: "ADD_USERS",
+                payload: data,
+            });
+        }
+
+        const localResponse = await fetch(
+            "https://striveschool-api.herokuapp.com/api/profile/me",
+            {
+                headers: {
+                    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzk5YmUwZDU0ZjRhYTAwMTUxOTMwMzkiLCJpYXQiOjE2NzEwMjAwNDUsImV4cCI6MTY3MjIyOTY0NX0.3XuEFOEGaNXNTq32t5nPT9qEUWuQJirhYW-2WA-luyQ`,
+                },
+            }
+        );
+
+        if (response.ok) {
+            const data = await localResponse.json();
+            dispatch({
+                type: "SET_USER",
+                payload: data,
+            });
+        }
+    }, []);
+    return (
+        <BrowserRouter>
+            <MyNavbar />
+            <Routes>
+                <Route path="/" element={<FeedPage />} />
+                <Route path="/in/:user_id" element={<ProfilePage />} />
+                <Route path="/search/:search" element={<SearchPage />} />
+            </Routes>
+        </BrowserRouter>
     );
-    if (response.ok)
-    {
-      const data = await response.json()
-      dispatch({
-        type: "ADD_USERS",
-        payload: data
-      })
-    }
-  }, []);
-  return (
-    <BrowserRouter>
-      <MyNavbar />
-      <Routes>
-        <Route path="/" element={<div />} />
-        <Route path="/in/:user_id" element={<ProfilePage />} />
-        <Route path="/search/:search" element={<SearchPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
 }
 
 export default App;
