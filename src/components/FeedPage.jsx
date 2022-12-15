845945537ccaa541f33d6ce2426a6d5676680d0f
Morgan <morgan@morganevans.dev>
2022-12-15 09:41:37 +0000

    fixed search

--- src/components/FeedPage.jsx
+++ src/components/FeedPage.jsx
@@ -1,7 +1,47 @@
 import React from "react";
-import { Row, Col, Container, Modal, Button, Form } from "react-bootstrap";
+import {
+    Row,
+    Col,
+    Container,
+    Modal,
+    Button,
+    Form,
+    Spinner,
+} from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { opts, uri } from "../Morgan";
 
 export default function FeedPage() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState("");
+    const [posts, setPosts] = useState([]);
+
+    const fetchPosts = async () => {
+        try {
+            const response = await fetch(
+                "https://striveschool-api.herokuapp.com/api/posts",
+                opts
+            );
+            if (response.ok) {
+                const data = await response.json();
+                setError("");
+                setIsLoading(false);
+                setPosts(data);
+            } else {
+                setError("Error fetching profile");
+                setIsLoading(false);
+            }
+        } catch (e) {
+            console.log(e);
+            setError("Error fetching profile");
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
     return (
         <Container>
             <Row>
@@ -9,7 +49,25 @@ export default function FeedPage() {
                     <h4>Left sidebar</h4>
                 </Col>
                 <Col md={6}>
-                    <h4>Col 2</h4>
+                    <h4>Feed</h4>
+                    <hr />
+                    {isLoading && <Spinner />}
+                    {!isLoading &&
+                        !error &&
+                        posts.slice(0, 25).map((post) => (
+                            <div
+                                style={{
+                                    background: "white",
+                                    padding: "1em",
+                                    marginBottom: "1em",
+                                }}
+                            >
+                                <p>{post.text}</p>
+                                <p>
+                                    by {post.user.name} {post.user.surname}
+                                </p>
+                            </div>
+                        ))}
                 </Col>
                 <Col md={3}>
                     <h4>Right sidebar</h4>
--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
 import { useParams } from "react-router-dom";
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function SearchPage() {
-    const {search} = useParams()
+    const { search } = useParams();
     const users = useSelector((state) => state.users);
     const searchedUser = users.filter((user) => {
-        return `${user.name} ${user.surname}`.includes(search);
-    })
-    console.log(searchedUser)
-  return (
-    <>
-    <div>{searchedUser.map((user) => (
-    <><Link to={`/in/${user._id}`}><p>{user.name} {user.surname}</p></Link><p>{user.email}</p></>))}</div>
-    </>
-  )
+        return `${user.name} ${user.surname}`.toLowerCase().includes(search);
+    });
+    console.log(searchedUser);
+    return (
+        <>
+            <div>
+                {searchedUser.map((user) => (
+                    <>
+                        <Link to={`/in/${user._id}`}>
+                            <p>
+                                {user.name} {user.surname}
+                            </p>
+                        </Link>
+                        <p>{user.email}</p>
+                    </>
+                ))}
+            </div>
+        </>
+    );
 }
