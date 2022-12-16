57965a0c0ee4518d8cf27dd37a0042097f980d56
Panna Szűcs <szucspann@gmail.com>
2022-12-16 01:14:04 +0100

    styling

--- src/components/FeedSidebar.jsx
+++ src/components/FeedSidebar.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
-import { Button, Container, Row } from 'react-bootstrap'
-
+import React from "react";
+import { Button, Container, Row } from "react-bootstrap";
 
 export default function FeedSidebar() {
   return (
-    <Container className='feedsidebar'>
-        
-            <h4>Add to your feed</h4>
+    <Container className="feedsidebar">
+      <h4>Add to your feed</h4>
 
-            <Row>
-                <img className='companylogo' src="" alt="companylogo" /> <div className='addfeedflex'><p>Company name</p><p>Type</p><Button className='follow'>Follow</Button></div>
-            </Row>
-        
-        
+      <Row>
+        <img className="companylogo" src="linkedinlogo.png" alt="companylogo" />{" "}
+        <div className="addfeedflex">
+          <p>Company name</p>
+          <p>Type</p>
+          <Button className="follow">Follow</Button>
+        </div>
+        </Row>
+        <Row>
+        <img className="companylogo" src="linkedinlogo.png" alt="companylogo" />
+        <div className="addfeedflex">
+          <p>Company name</p>
+          <p>Type</p>
+          <Button className="follow">Follow</Button>
+        </div> 
+        </Row>
+        <Row>
+        <img className="companylogo" src="linkedinlogo.png" alt="companylogo" /><div className="addfeedflex">
+          <p>Company name</p>
+          <p>Type</p>
+          <Button className="follow">Follow</Button>
+        </div>
+        </Row>
+      
     </Container>
-  )
+  );
 }
