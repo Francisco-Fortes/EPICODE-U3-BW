d2f8342aa066f7de836576bada7ff2bd081eb439
Morgan <morgan@morganevans.dev>
2022-12-16 08:36:38 +0000

    Fixed feed sidebars CSS

--- src/components/FeedSidebar.jsx
+++ src/components/FeedSidebar.jsx
@@ -2,34 +2,52 @@ import React from "react";
 import { Button, Container, Row } from "react-bootstrap";
 
 export default function FeedSidebar() {
-  return (
-    <Container className="feedsidebar">
-      <h4>Add to your feed</h4>
+    return (
+        <Container className="feedsidebar">
+            <h4>Add to your feed</h4>
 
-      <Row>
-        <img className="companylogo" src="linkedinlogo.png" alt="companylogo" />{" "}
-        <div className="addfeedflex">
-          <p>Company name</p>
-          <p>Type</p>
-          <Button className="follow">Follow</Button>
-        </div>
-        </Row>
-        <Row>
-        <img className="companylogo" src="linkedinlogo.png" alt="companylogo" />
-        <div className="addfeedflex">
-          <p>Company name</p>
-          <p>Type</p>
-          <Button className="follow">Follow</Button>
-        </div> 
-        </Row>
-        <Row>
-        <img className="companylogo" src="linkedinlogo.png" alt="companylogo" /><div className="addfeedflex">
-          <p>Company name</p>
-          <p>Type</p>
-          <Button className="follow">Follow</Button>
-        </div>
-        </Row>
-      
-    </Container>
-  );
+            <Row>
+                <div className="addfeedflex2">
+                    <img
+                        className="companylogo"
+                        src="linkedinlogo.png"
+                        alt="companylogo"
+                    />
+                    <div className="addfeedflex">
+                        <p>Company name</p>
+                        <p>Type</p>
+                        <Button className="follow">Follow</Button>
+                    </div>
+                </div>
+            </Row>
+            <Row>
+                <div className="addfeedflex2">
+                    <img
+                        className="companylogo"
+                        src="linkedinlogo.png"
+                        alt="companylogo"
+                    />
+                    <div className="addfeedflex">
+                        <p>Company name</p>
+                        <p>Type</p>
+                        <Button className="follow">Follow</Button>
+                    </div>
+                </div>
+            </Row>
+            <Row>
+                <div className="addfeedflex2">
+                    <img
+                        className="companylogo"
+                        src="linkedinlogo.png"
+                        alt="companylogo"
+                    />
+                    <div className="addfeedflex">
+                        <p>Company name</p>
+                        <p>Type</p>
+                        <Button className="follow">Follow</Button>
+                    </div>
+                </div>
+            </Row>
+        </Container>
+    );
 }
--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import { Container, Row } from 'react-bootstrap'
-
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 export default function LeftSidebar() {
-  return (
-    <Container className='leftsidebar'>
-        <div> <img src="" alt="" /> </div>
-        <div> <img src="" alt="" /> </div>
-        <div><h4>Welcome, name!</h4>
-        <hr/> 
-        <div className='connflex'><p>Connections</p> <p><a href="">0</a></p></div>
-        <hr />
-        <a className='premiumlink' href="">Try premium for free</a>
-        <hr />
-        <p><a href="">My items</a></p>
-    </div>
-    </Container>
-  )
+    const localUser = useSelector((state) => state.activeUser);
+    return (
+        <Container className="leftsidebar">
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <h4>Welcome, {localUser?.name}!</h4>
+                <hr />
+                <div className="connflex">
+                    <p>Connections</p>
+                    <p>
+                        <a href="">0</a>
+                    </p>
+                </div>
+                <hr />
+                <a className="premiumlink" href="">
+                    Try premium for free
+                </a>
+                <hr />
+                <p>
+                    <a href="">My items</a>
+                </p>
+            </div>
+        </Container>
+    );
 }
