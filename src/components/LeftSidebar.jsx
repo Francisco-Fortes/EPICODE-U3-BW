85c9eee0eec5769c36d141dffdba495df4cf7fbf
Panna Szűcs <szucspann@gmail.com>
2022-12-16 01:42:47 +0100

    styling

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -9,7 +9,11 @@ export default function LeftSidebar() {
         <div> <img src="" alt="" /> </div>
         <div><h4>Welcome, name!</h4>
         <hr/> 
-        <p>Connections</p> <p><a href="">0</a></p>
+        <div className='connflex'><p>Connections</p> <p><a href="">0</a></p></div>
+        <hr />
+        <a className='premiumlink' href="">Try premium for free</a>
+        <hr />
+        <p><a href="">My items</a></p>
     </div>
     </Container>
   )
